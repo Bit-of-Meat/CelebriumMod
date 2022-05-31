10c121a6ccd7e0ba8f7779da6ce5a7320f8a5128
Max <qbite0@gmail.com>
2022-06-01 01:24:09 +0700

    remove unused libs

--- Tiles/CelebriumOre.cs
+++ Tiles/CelebriumOre.cs
@@ -1,11 +1,5 @@
 using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria;
-using Terraria.Localization;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
